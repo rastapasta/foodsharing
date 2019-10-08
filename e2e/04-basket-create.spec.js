dba4415e80ee816f93e92f100d1d73f1f917a323
Michael Straßburger <codepoet@cpan.org>
2019-10-08 22:34:46 +0200

    :white_check_mark: improving naming of basket tests

--- e2e/04-basket-create.spec.js
+++ e2e/04-basket-create.spec.js
@@ -5,87 +5,97 @@ const description = 'This is a test basket created by the CI'
 
 describe('Baskets list and creation', () => {
 
-  it('switch to baskets list by tapping bottom navigation', async () => {
+  it('should navigate to baskets when pressing navigation tab icon', async () => {
     await element(by.id('navigation.basket')).atIndex(0).tap()
     await expect(element(by.id('baskets.scene'))).toBeVisible()
   })
 
-  it('tap plus button to add new basket', async () => {
+  it('should navigate to offer basket scene when tapping plus button', async () => {
     await element(by.id('navigation.plus')).tap()
     await expect(element(by.id('basketEdit.scene'))).toBeVisible()
   })
 
-  it('press camera button and expect camera view', async () => {
+  it('should open the camera view when pressing the camera icon', async () => {
     await element(by.id('basketEdit.camera')).tap()
     await expect(element(by.id('camera.scene'))).toBeVisible()
   })
 
-  it('take a picture and get redirected back', async () => {
+  it('should take a picture and navigate back to basket when pressing action button', async () => {
     await element(by.id('camera.action')).tap()
     await expect(element(by.id('basketEdit.scene'))).toBeVisible()
   })
 
-  it('description field is changeable', async () => {
+  it('should be possible to enter text into description field', async () => {
     await element(by.id('basketEdit.description')).tap()
     await element(by.id('basketEdit.description')).typeText(description)
   })
 
-  it('leaving the description field by tapping ouside of field', async () => {
+  it('sould be possible to leave the field by tapping the picture', async () => {
     await element(by.id('basketEdit.picture')).tap()
   })
 
-  it('checkboxes are chooseable and toggle options', async () => {
+  it('should not display dynamic content until checkboxes toggled', async () => {
     await expect(element(by.id('basketEdit.mobile'))).toBeNotVisible()
     await expect(element(by.id('basketEdit.landline'))).toBeNotVisible()
+  })
 
+  it('should be possible to toggle the checkboxes', async () => {
     await element(by.id('basketEdit.by_message')).tapAtPoint({x: 12, y: 15})
     await element(by.id('basketEdit.by_phone')).tap()
+  })
 
+  it('should display dynamic fields after toggle', async () => {
     await element(by.id('basketEdit.mobile')).swipe('up')
 
     await expect(element(by.id('basketEdit.mobile'))).toBeVisible()
     await expect(element(by.id('basketEdit.landline'))).toBeVisible()
   })
 
-  it('mobile text field accepts entry', async () => {
+  it('should be possible to enter mobile number', async () => {
     await element(by.id('basketEdit.mobile')).tap()
     await element(by.id('basketEdit.mobile')).typeText('0171 1234567')
   })
 
-  it('dropbox opens and allows selection', async () => {
+  it('shoule possible to open dropbox', async () => {
     await element(by.id('basketEdit.dropdown')).tap()
     await element(by.id('basketEdit.dropdown')).tap()
     await expect(element(by.label(translation.baskets.until_tomorrow))).toBeVisible()
+  })
+
+  it('should be possible to select dropbox item', async () => {
     await element(by.label(translation.baskets.only_today)).tap()
   })
 
-  it('map is tapable and opens location selector', async () => {
+  it('should navigate to location selector when tapping map', async () => {
     await element(by.id('basketEdit.map')).tap()
     await expect(element(by.id('locationSelector.scene'))).toBeVisible()
   })
 
-  it('map is moveable and position gets returned back', async () => {
+  it('should be able to swipe map and get result back to basket', async () => {
     await element(by.id('locationSelector.map')).swipe('down')
     await element(by.id('locationSelector.map')).swipe('left')
     await element(by.id('header-back')).atIndex(0).tap()
   })
 
-  it('publishing the basket', async () => {
+  it('should publish basket when tapping publish button', async () => {
     await element(by.id('basketEdit.submit')).tap()
   })
 
-  it('should get redirected to basket page for fresh basket', async () => {
+  it('should navigate to basket page with fresh basket', async () => {
     await expect(element(by.id('basket.scene'))).toBeVisible()
     await expect(element(by.id('basket.description'))).toHaveText(description)
   })
 
-  it('back button should lead to baskets overview, new basket visible there', async () => {
+  it('should navigate to baskets overview when tapping back', async () => {
     await element(by.id('header-back')).atIndex(0).tap()
     await expect(element(by.id('baskets.scene'))).toBeVisible()
+  })
+
+  it('should show new basket in overview', async () => {
     await expect(element(by.id('baskets.label.0'))).toHaveText(description)
   })
 
-  it('tapping on basket should bring you back to basket', async () => {
+  it('should bring you back to basket when tapping on it', async () => {
     await element(by.id('baskets.0')).tap()
     await expect(element(by.id('basket.scene'))).toBeVisible()
     await expect(element(by.id('basket.description'))).toHaveText(description)
--- e2e/05-basket-edit.spec.js
+++ e2e/05-basket-edit.spec.js
@@ -4,47 +4,57 @@ require('custom-env').env()
 const editedDescription = 'This is an edited basket created by the CI'
 
 describe('Basket edit scene', () => {
-  it('tapping on person leads to profile', async () => {
+  it('should navigate user to profile when tapping person', async () => {
     await element(by.id('basket.creator')).tap()
     await expect(element(by.id('profile.scene'))).toBeVisible()
-    await element(by.id('navigation.back')).tap()
   })
 
-  it('tapping on edit leads to the edit basket page', async () => {
+  if('should navigate back to basket when tapping back', async () => {
+    await element(by.id('navigation.back')).tap()
+    await expect(element(by.id('basket.scene'))).toBeVisible()
+  })
+
+  it('should navigate to edit basket when tapping edit button', async () => {
     await element(by.id('basket.edit')).tap()
     await expect(element(by.id('basketEdit.scene'))).toBeVisible()
   })
 
-  it('updating the description works as expected', async () => {
+  it('should be able to edit description text', async () => {
     await element(by.id('basketEdit.description')).tap()
     await element(by.id('basketEdit.description')).replaceText('')
     await element(by.id('basketEdit.description')).typeText(editedDescription)
+    await element(by.id('basketEdit.map')).tap()
   })
 
-  it('tapping the mini map loads the location selector', async () => {
-    await element(by.id('basketEdit.map')).tap()
+  it('should navigate to location selector when tapping map', async () => {
     await element(by.id('basketEdit.map')).tap()
     await expect(element(by.id('locationSelector.scene'))).toBeVisible()
   })
 
-  it('map is moveable and position gets returned back', async () => {
+  it('should be able to swipe map and get result back to basket', async () => {
     await element(by.id('locationSelector.map')).swipe('down')
     await element(by.id('locationSelector.map')).swipe('left')
     await element(by.id('header-back')).atIndex(0).tap()
   })
 
-  it('changes are saveable, redirect happens and changes get displayed', async () => {
+  it('should be able to save and get redirecte to basket', async () => {
     await element(by.id('basketEdit.submit')).tap()
     await expect(element(by.id('basket.scene'))).toBeVisible()
+  })
+
+  it('should display the new basket description', async () => {
     await expect(element(by.id('basket.description'))).toHaveText(editedDescription)
   })
 
-  it('delete basket and get navigated back to baskets', async () => {
+  it('should delete basket when tapping delete', async () => {
     await element(by.id('basket.delete')).tap()
+  })
+
+  it('should navigate back after deletion', async () => {
     await expect(element(by.id('baskets.scene'))).toBeVisible()
   })
 
-  it('baskets list should be empty again', async () => {
+  it('should be an empty basket list again', async () => {
     await expect(element(by.id('baskets.0'))).toNotExist()
   })
 })
