8fd84e240901c9af20c2811cbf3576cededa675b
Michael Straßburger <codepoet@cpan.org>
2019-10-09 07:11:05 +0200

    :zap: optimizing rendering for Profile, -circles, -buttons, Conversation, ...

--- src/scenes/Profile.tsx
+++ src/scenes/Profile.tsx
@@ -1,9 +1,7 @@
-import { withNavigationFocus } from 'react-navigation'
-
-import React, { PureComponent, Fragment } from 'react'
-import { View, StyleSheet, Dimensions, Text, TouchableOpacity, ActivityIndicator, Platform } from 'react-native'
+import React, { Component, Fragment } from 'react'
+import { View, StyleSheet, Dimensions, Text, ActivityIndicator, Platform } from 'react-native'
 import { isIphoneX } from 'react-native-iphone-x-helper'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { withNavigationFocus } from 'react-navigation'
 
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
@@ -16,6 +14,7 @@ import ProfileCircle from '../components/ProfileCircle'
 
 import { foodsaver } from '../common/placeholder'
 import BackButton from '../components/BackButton'
+import ProfileButton from '../components/ProfileButton'
 import colors from '../common/colors'
 
 import { Actions } from 'react-native-router-flux'
@@ -26,9 +25,9 @@ const { width, height } = Dimensions.get('window')
 const avatar = 'https://foodsharing.de/img/130_q_avatar.png'
     , hat = 'https://beta.foodsharing.de/img/sleep130x130.png'
     , circles = [
-      {name: 'bananacount', unit: ''},
-      {name: 'basketcount', unit: ''},
-      {name: 'postcount', unit: ''},
+      {name: 'bananacount'},
+      {name: 'basketcount'},
+      {name: 'postcount'},
       {name: 'fetchcount', unit: 'x'},
       {name: 'fetchweight', unit: 'kg'}
     ]
@@ -54,19 +53,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     alignItems: 'center',
   },
-  button: {
-    flex: 1,
-    padding: 6,
-    marginLeft: 8,
-    marginRight: 8,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  buttonText: {
-    textAlign: 'center',
-    fontSize: 10,
-    color: colors.profileButton
-  },
   header: {
     width,
     backgroundColor: colors.background,
@@ -88,6 +74,11 @@ const styles = StyleSheet.create({
       {translateY: -35},
       {translateX: -25}
     ]
+  },
+  circles: {
+    flexDirection: 'row',
+    marginBottom: 10,
+    justifyContent: 'space-evenly'
   }
 })
 
@@ -100,7 +91,14 @@ type Props = {
   isFocused: boolean
 }
 
-class Profile extends PureComponent<Props> {
+class Profile extends Component<Props> {
+  shouldComponentUpdate(next: Props) {
+    const { id, foodsavers, profile } = this.props
+    return next.id !== id
+        || next.profile.id !== profile.id
+        || next.foodsavers[id] !== foodsavers[id]
+  }
+
   componentDidUpdate(prevProps: Props) {
     const { actions, id } = this.props
     if (prevProps.isFocused === false && this.props.isFocused === true)
@@ -118,21 +116,6 @@ class Profile extends PureComponent<Props> {
         , profile = foodsaver(foodsavers[`${id}`])
         , hasStats = profile.stats && !Object.keys(profile.stats).every(stat => profile.stats[stat] === 0)
         , headerHeight = (isIphoneX() ? 155 : 125) - (!hasStats && !profile.loading ? 65 : 0)
-
-    const Button = ({icon, label, color, onPress, disabled, loading}) =>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={onPress}
-        disabled={disabled || loading}
-      >
-        {loading ?
-          <View style={{width: 28, height: 28, alignItems: 'center', justifyContent: 'center'}}>
-            <ActivityIndicator size="small" color={color} />
-          </View> :
-          <Icon name={icon} size={26} color={color}/>
-        }
-        <Text style={[styles.buttonText, {color}]}>{label}</Text>
-      </TouchableOpacity>
 
     return (
       <View style={styles.container} testID='profile.scene'>
@@ -153,7 +136,11 @@ class Profile extends PureComponent<Props> {
                 </Text>
               </View>
               <View style={styles.circleBar} pointerEvents='box-none'>
-                {profile.loading && !hasStats && <View style={{justifyContent: 'center', height: 50}}><ActivityIndicator size="small" color={colors.white} /></View>}
+                {profile.loading && !hasStats &&
+                  <View style={{justifyContent: 'center', height: 50}}>
+                    <ActivityIndicator size="small" color={colors.white} />
+                  </View>
+                }
                 {hasStats && circles.map(circle =>
                   !!profile.stats[circle.name] &&
                   <ProfileCircle
@@ -170,8 +157,8 @@ class Profile extends PureComponent<Props> {
         >
           <View style={{minHeight: height - (Platform.OS === 'ios' ? headerHeight : headerHeight +  24), backgroundColor: colors.white, marginTop: 5, padding: 20}}>
             {user.id !== profile.id &&
-              <View style={{flexDirection: 'row', marginBottom: 10, justifyContent: 'space-evenly'}}>
-                <Button
+              <View style={styles.circles}>
+                <ProfileButton
                   label={translate(profile.isFriend ? 'profile.you_know_person' : 'profile.i_know_person', {name: profile.name})}
                   icon={profile.isFriend ?  'account-check-outline' : 'account-plus-outline'}
                   color={profile.isFriend ? colors.profileButtonFriend : colors.profileButton}
@@ -179,7 +166,7 @@ class Profile extends PureComponent<Props> {
                   disabled={!!profile.isFriend}
                   loading={profile.friendrequestLoading}
                 />
-                <Button
+                <ProfileButton
                   label={translate('profile.write_message')}
                   icon="message-text-outline"
                   onPress={() => actions.fetchConversationId(profile.id)}
@@ -187,7 +174,7 @@ class Profile extends PureComponent<Props> {
                   disabled={false}
                   loading={profile.conversationIdLoading}
                 />
-                <Button
+                <ProfileButton
                   label={translate(profile.reported && !profile.reportSending ? 'profile.reported' : 'profile.report_violation')}
                   icon={profile.reported ? 'shield-check-outline' : 'alert-decagram-outline'}
                   onPress={() => Actions.push('report', {id: profile.id})}
