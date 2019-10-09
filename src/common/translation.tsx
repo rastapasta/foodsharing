8ae2b472e506060bbf91b96b6967f339a92b431e
Michael Straßburger <codepoet@cpan.org>
2019-10-09 02:10:04 +0200

    :globe_with_meridians: enabling german translation, localizing moment

--- src/common/translation.tsx
+++ src/common/translation.tsx
@@ -3,11 +3,12 @@ import * as RNLocalize from "react-native-localize"
 import i18n from "i18n-js"
 import memoize from "lodash.memoize"
 
-// import moment from 'moment'
-// import 'moment/min/moment-with-locales'
+import moment from 'moment'
+import 'moment/min/locales'
 
 const translationGetters = {
   en: () => require("../../assets/translations/en.json"),
+  de: () => require("../../assets/translations/de.json")
 }
 
 export const translate = memoize(
@@ -30,7 +31,8 @@ const setI18nConfig = () => {
 
   i18n.translations = { [languageTag]: translationGetters[languageTag]() }
   i18n.locale = languageTag
-  // moment.locale([languageTag, 'en'])
+
+  moment.locale(languageTag)
 }
 
 setI18nConfig()
